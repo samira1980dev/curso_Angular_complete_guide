22f542e921896280e9505bae1d93586b9e50ff8c
Samira Desktop <samira1980dev@gmail.com>
2022-05-17 17:36:19 -0300

    algumas comunicacoes do app

--- FirstProject/first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FirstProject/first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://img.taste.com.au/nS9XlWPQ/w733-h489-cfill-q80/taste/2017/08/steak-with-mushroom-sauce-129161-2.jpg')
+  ];
 
   constructor() { }
 
